a06901edafc7667664d3037a8e38380a30f4903c
Yolanda <yolanda.collins@firemind.io>
2020-12-18 14:06:42 +0000

    Added welcome screen with background and temporary logo

---
+++ app/screens/WelcomeScreen.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { ImageBackground, StyleSheet, View, Image } from "react-native";
+
+import Screen from "../components/Screen";
+
+function WelcomeScreen(props) {
+  return (
+    <ImageBackground
+      style={styles.background}
+      source={require("../assets/welcomeScreen.jpg")}
+    >
+    <Screen style={styles.screen}>
+      <View style={styles.logoContainer}>
+        <Image style={styles.logo} source={require("../assets/bearFitness.png")} />
+      </View>
+    </Screen>
+      
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  screen: {
+    width: "100%",
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  buttonsContainer: {
+    padding: 20,
+    width: "100%",
+  },
+  logo: {
+    width: 150,
+    height: 100,
+  },
+  logoContainer: {
+    position: "absolute",
+    top: 0,
+    alignItems: "center",
+  },
+});
+
+export default WelcomeScreen;
