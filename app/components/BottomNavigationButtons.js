796487c82a7d9132686c08dd22a00cdb230728db
Yolanda <yolanda.collins@firemind.io>
2021-01-08 17:24:47 +0000

    Created BottomNavigationButtons component

--- app/components/BottomNavigationButtons.js
+++ app/components/BottomNavigationButtons.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
-const BottomNavigationButtons = ({ navigation, screenName }) => {
+import AppBlock from '../components/AppBlock'
+import AppButtonBasic from '../components/AppButtonBasic'
+
+const BottomNavigationButtons = ({ navigation, screenName, backName }) => {
   return (
-    <div>
-      
-    </div>
+    <AppBlock>
+      <AppButtonBasic title='Back' onPress={() => navigation.navigate(backName) }/>
+      <AppButtonBasic title='Next' onPress={() => navigation.navigate(screenName) }/>
+    </AppBlock>
   )
 }
+
+export default BottomNavigationButtons
