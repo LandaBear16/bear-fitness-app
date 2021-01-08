e9abfb91800596e21e7d48008b80d3c0e7ea6aae
Yolanda <yolanda.collins@firemind.io>
2021-01-08 15:06:26 +0000

    Added UserContext as this will need to be used throughout the app

--- App.js
+++ App.js
@@ -17,7 +17,7 @@ export default function App() {
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <Levels />
+        <AppStackScreens />
       </NavigationContainer>
     </Provider>
   );
